5e598845a5d0769bc8c5e6dce9d8dfb3b055036a
ishitasavaliya <141223316+ishitasavaliya@users.noreply.github.com>
2024-02-04 10:25:26 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,9 @@
+public class Random {
+    public static void main(String[] args) {
+       
+        double randomNumber = Math.random();
+
+        
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
