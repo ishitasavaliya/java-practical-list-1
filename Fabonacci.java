5b5f8098ce26268be0ddb9fbdbe1863ec7013c38
ishitasavaliya <141223316+ishitasavaliya@users.noreply.github.com>
2024-02-05 18:34:24 +0530

    Fabonacci.java

--- Fabonacci.java
+++ Fabonacci.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Fibonacci{
+	public static void main(String[] s)
+{
+	int i,n,a=0,b=1,c;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the n number:");
+	n=sc.nextInt();
+	
+	for(i=1;i<=n;i++){
+		System.out.print(" "+a);
+		c=a+b;
+		a=b;
+		b=c;
+	}
+}
+}
