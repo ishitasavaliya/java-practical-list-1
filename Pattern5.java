fce9195d0a92820f398d026ab13777fdd606bfd1
ishitasavaliya <141223316+ishitasavaliya@users.noreply.github.com>
2024-02-08 20:51:37 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,14 @@
+public class Pattern5{
+
+	public static void main(String s[]){
+	
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=7;j>=i;j--)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
