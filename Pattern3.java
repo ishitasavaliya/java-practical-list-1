960ab6b067f6b4682fcf070e226dd08e51ba8d34
ishitasavaliya <141223316+ishitasavaliya@users.noreply.github.com>
2024-02-08 20:44:30 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+public class Pattern3{
+	public static void main(String s[]){
+		for(int i=1;i<=6;i++)
+		{
+			for(int j=6;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+}
+}
